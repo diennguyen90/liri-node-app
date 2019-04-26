fabe12b2deefc16c72ab69238140b20e2491a804
dien <iamdiennn@gmail.com>
2019-04-25 18:07:36 -0700

    concer-this query

--- liri.js
+++ liri.js
@@ -28,7 +28,15 @@ const getMovie = () =>{
 const getConcert = _ =>{
     axios.get(`https://rest.bandsintown.com/artists/${x}/events?app_id=codingbootcamp`)
         .then(r => {
-            console.log(r.data)
+            let data = r.data
+            // console.log(data[0].venue)
+            // console.log(r.data[0].venue)
+            for(let i = 0;i < data.length;i++){
+                console.log(`
+                Venue: ${data[i].venue.name}
+                Location: ${data[i].venue.city}, ${data[i].venue.country}
+                `)
+            }
         }).catch(e => console.log(e))
 }
 
